6d2e564e408e18fc72a3406c65fd1fb64a74ec11
minhnn <minh.nn.00000@gmail.com>
2025-01-23 10:52:19 +0700

    update map

---
+++ scripts/outside.lua
@@ -0,0 +1,9 @@
+local outside = {}
+
+local clouds = {}
+
+local clouds_srcs = {
+	{x = 0, y = 0, w = 121, h = 51},
+	{x = 124, y = 0, w = 41, h = 24},
+	{x = 121, y = , w = 121, h = 51},
+	{x = 0, y = 0, w = 121, h = 51},
