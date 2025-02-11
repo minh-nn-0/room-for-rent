9d6030ea1f37be397a155540c90c4a542250b54b
minhnn <minh.nn.00000@gmail.com>
2025-02-11 18:06:01 +0700

    small change

--- gamedata/scripts/audio/ambience.lua
+++ gamedata/scripts/audio/ambience.lua
@@ -9,10 +9,15 @@ local sounds_per_location = {
 				{"carpassing",10,0.3},
 				{"dog",15,0.3}
 			}
-		}
+		},
+		{
+			ambience = {"ambience_wind", "ambience_cricket"},
+		},
 	},
 	["Map.Mainroom"] = {
-		{},
+		{
+			ambience = {"ambience_bird"}
+		},
 		{
 			ambience = {"ambience_cricket"},
 		}
@@ -26,42 +31,41 @@ local function fade_out_ambience()
 	current_sound_channels = {}
 end
 
-local function fade_in_ambience(location)
-	for _,sounds in ipairs(sounds_per_location[location]) do
-		if sounds.ambience then
-			for _,sound in ipairs(sounds.ambience) do
-				table.insert(current_sound_channels, beaver.fade_in_channel(sound, -1, -1, 1000))
-			end
+local function fade_in_ambience(location, tod)
+	local sounds = sounds_per_location[location][tod]
+	if sounds.ambience then
+		for _,sound in ipairs(sounds.ambience) do
+			table.insert(current_sound_channels, beaver.fade_in_channel(sound, -1, -1, 1000))
 		end
 	end
 end
 
 local played_sounds = {}
-local function play_random_sounds(location)
-	for _,sounds in ipairs(sounds_per_location[location]) do
-		if sounds.random_sound then
-			for _,sound in ipairs(sounds.random_sound) do
-				if math.floor(beaver.get_elapsed_time() % sound[2]) == 1 and math.random() < sound[3]
-						and not played_sounds[sound[1]] then
-					played_sounds[sound[1]] = beaver.play_sound(sound[1])
-				end
+local function play_random_sounds(location, tod)
+	local sounds = sounds_per_location[location][tod]
+	if sounds.random_sound then
+		for _,sound in ipairs(sounds.random_sound) do
+			if math.floor(beaver.get_elapsed_time() % sound[2]) == 1 and math.random() < sound[3]
+					and not played_sounds[sound[1]] then
+				played_sounds[sound[1]] = beaver.play_sound(sound[1])
 			end
+		end
 
-			for i,channel in pairs(played_sounds) do
-				if not beaver.channel_playing(channel) then played_sounds[i] = nil end
-			end
+		for i,channel in pairs(played_sounds) do
+			if not beaver.channel_playing(channel) then played_sounds[i] = nil end
 		end
 	end
 end
 function ambience.update()
 	local plocation = rfr.get_location(PLAYER)
+	local _,tod = rfr.current_time()
 	if sounds_per_location[plocation] ~= nil then
 		if current_location ~= plocation then
 			current_location = plocation
 			fade_out_ambience()
-			fade_in_ambience(current_location)
+			fade_in_ambience(current_location, tod)
 		else
-			play_random_sounds(current_location)
+			play_random_sounds(current_location, tod)
 		end
 	end
 end
